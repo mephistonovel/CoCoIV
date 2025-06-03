e5b55359fa80d9cd7d2422c21c5069deedad7974
mephistonovel <101092084+mephistonovel@users.noreply.github.com>
2025-06-03 22:29:16 +0900

    Update experiment_extended.sh

--- experiment_extended.sh
+++ experiment_extended.sh
@@ -1,7 +1,7 @@
 model="ours"
 size=5000
 
-    # Ablation studies (Sec.5.2)
+    # Ablation studies (Sec.V.B)
     python main.py --feature-dim 6 --latent-dim 2 --latent-dim-t 3 --hidden-dim 200 --sample_size $size --batch_size 512 --treatment b --comp-num-zc 4 --hyp_mi 5 --model_id $model --response linear --exp_id abl --true_effect 3 --repetition 20 --highdim 0
     python main.py --feature-dim 6 --latent-dim 2 --latent-dim-t 3 --hidden-dim 200 --sample_size $size --batch_size 512 --treatment b --comp-num-zc 4 --hyp_mi 5 --model_id $model --response nonlinear --exp_id abl --true_effect 3 --repetition 20 --highdim 0
     python main.py --feature-dim 6 --latent-dim 2 --latent-dim-t 3 --hidden-dim 200 --sample_size $size --batch_size 512 --treatment con --comp-num-zc 4 --hyp_mi 5 --model_id $model --response linear  --exp_id abl --true_effect 3 --repetition 20 --highdim 0
